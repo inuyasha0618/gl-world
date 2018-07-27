57d730d0c5317b721754b23358799638555a40c4
inuyasha0618 <chenjiandiwang@qq.com>
2018-07-27 22:44:09 +0800

    add getGlInstance

---
+++ js/getGlInstance.js
@@ -0,0 +1,29 @@
+export default function(canvasId) {
+    let canvas = document.getElementById(canvasId);
+    let gl = canvas.getContext('webgl2');
+
+    if (!gl) {
+        console.error('Webgl context is not available.');
+        return null;
+    }
+
+    gl.clearColor(0.0, 1.0, 1.0, 1.0);
+
+    const glClear = gl.clear.bind(gl);
+
+    gl.clear = function() {
+        glClear(this.COLOR_BUFFER_BIT | this.DEPTH_BUFFER_BIT);
+        return this;
+    }
+
+    gl.setSize = function(w=500, h=500) {
+        this.canvas.style.width = w + 'px';
+        this.canvas.style.height = h + 'px';
+        this.canvas.width = w;
+        this.canvas.height = h;
+        this.viewport(0, 0, w, h);
+        return this;
+    }
+
+    return gl;
+}
---
+++ js/index.js
@@ -0,0 +1,4 @@
+import getGlInstance from './getGlInstance.js';
+
+let gl = getGlInstance('cvs-id').setSize(600, 600);
+gl.clear();
