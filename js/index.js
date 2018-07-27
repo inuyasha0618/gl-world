eda55124d06ec502cef40aa0d265fcc5b0fe1b5f
inuyasha0618 <chenjiandiwang@qq.com>
2018-07-28 00:02:06 +0800

    first step, draw a point

--- js/index.js
+++ js/index.js
@@ -1,3 +1,21 @@
 import getGlInstance from './getGlInstance.js';
+import { getProgramObj } from './shaderUtils.js';
 
 let gl = getGlInstance('cvs-id').setSize(600, 600).clearColor(0.0, 0.0, 0.0, 1.0).clear();
+let program = getProgramObj(gl, 'vertex-shader', 'fragment-shader');
+
+console.log(`program: ${program}`);
+
+gl.useProgram(program);
+let positionIndex = gl.getAttribLocation(program, 'a_position');
+let vertexBuffer = gl.createBuffer();
+
+let positions = new Float32Array([0.0, 0.0, 0.0]);
+
+gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+gl.bufferData(gl.ARRAY_BUFFER, positions, gl.STATIC_DRAW);
+
+gl.enableVertexAttribArray(positionIndex);
+gl.vertexAttribPointer(positionIndex, 3, gl.FLOAT, false, 0, 0);
+
+gl.drawArrays(gl.POINTS, 0, 1);
---
+++ js/shaderUtils.js
@@ -0,0 +1,59 @@
+const getShaderSrc = scriptId => {
+    let scriptNode = document.getElementById(scriptId);
+    if (!scriptNode) {
+        console.error(`${scriptId} not found`);
+        return null;
+    }
+
+    if (scriptNode.text.trim() == '') {
+        console.error(`${scriptId} has no content`);
+        return null;
+    }
+
+    return scriptNode.text.trim();
+}
+
+const getShaderObj = (gl, shaderSrc, type) => {
+
+    let shader = gl.createShader(type);
+    gl.shaderSource(shader, shaderSrc);
+    gl.compileShader(shader);
+
+    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+        console.error(`Error compiling shader : ${shaderSrc} ${gl.getShaderInfoLog(shader)}`);
+        gl.deleteShader(shader);
+        return null;
+    }
+
+    return shader;
+}
+
+const createProgram = (gl, vShader, fShader) => {
+    let program = gl.createProgram();
+    gl.attachShader(program, vShader);
+    gl.attachShader(program, fShader);
+    gl.linkProgram(program);
+
+    if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+        console.error(`Error creating shader program ${gl.getProgramInfoLog(program)}`);
+        gl.deleteProgram(program);
+        return null;
+    }
+
+    gl.detachShader(program, vShader);
+    gl.detachShader(program, fShader);
+    gl.deleteShader(vShader);
+    gl.deleteShader(fShader);
+
+    return program;
+}
+
+export const getProgramObj = (gl, vsId, fsId) => {
+    let vShaderSource = getShaderSrc(vsId);                             if (!vShaderSource) return null;
+    let fShaderSource = getShaderSrc(fsId);                             if (!fShaderSource) return null;
+
+    let vShader = getShaderObj(gl, vShaderSource, gl.VERTEX_SHADER);    if (!vShader) return null;
+    let fShader = getShaderObj(gl, fShaderSource, gl.FRAGMENT_SHADER);  if (!fShader) return null;
+
+    return createProgram(gl, vShader, fShader);
+}
