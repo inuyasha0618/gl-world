11e3b406dea2cf0127cb4a41efed85d79c185695
mastersun <mastersun@example.com>
2018-08-02 16:27:53 +0800

    update Modal.js

--- js/Modal.js
+++ js/Modal.js
@@ -4,4 +4,46 @@ export default class Modal {
         this.mesh = meshData;
         this.transform = new Transform();
     }
+
+    setScale(x, y, z) {
+        this.transform.scale.set(x, y, z);
+        this.transform.updateMatrix();
+        return this;
+    }
+    
+    setPosition(x, y, z) {
+        this.transform.position.set(x, y, z);
+        this.transform.updateMatrix();
+        return this;
+    }
+
+    setRotation(x, y, z) {
+        this.transform.rotation.set(x, y, z);
+        this.transform.updateMatrix();
+        return this;
+    }
+
+    addScale(dx, dy, dz) {
+        this.transform.scale.x += dx;
+        this.transform.scale.y += dy;
+        this.transform.scale.z += dz;
+        this.transform.updateMatrix();
+        return this;
+    }
+
+    addPosition(dx, dy, dz) {
+        this.transform.position.x += dx;
+        this.transform.position.y += dy;
+        this.transform.position.z += dz;
+        this.transform.updateMatrix();
+        return this;
+    }
+
+    addRotation(dx, dy, dz) {
+        this.transform.rotation.x += dx;
+        this.transform.rotation.y += dy;
+        this.transform.rotation.z += dz;
+        this.transform.updateMatrix();
+        return this;
+    }
 }
